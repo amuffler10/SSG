3180a05eef628a1b8e3b5c9d151a597d4369e838
amuffler10 <austinmuffler@yahoo.com>
2024-08-27 20:56:20 -0500

    added textnode to htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,14 +21,29 @@ class HTMLNode:
     
 class LeafNode(HTMLNode):
 
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value == None:
             raise ValueError
-        if self.tag == None:
+        elif self.tag == None:
             return self.value
         else:
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-    
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("tag is None")
+        elif self.children == None:
+            raise ValueError("children is None")
+        else:
+            children_to_html = ""
+            for node in self.children:
+                children_to_html += node.to_html()
+            return f"<{self.tag}{self.props_to_html()}>{children_to_html}</{self.tag}>"
--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ from textnode import TextNode
 from htmlnode import *
 
 def main():
-    node_a = LeafNode("p", "This is a paragraph of text.")
-    node_b = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    
-    print(node_a.to_html())
-    print("-------------------")
-    print(node_b.to_html())
+    node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
+
+    print(node.to_html())
 
 main()
 
