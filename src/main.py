19e3512f4faf2f95f1d14c18f3abc53e4559012a
amuffler10 <austinmuffler@yahoo.com>
2024-08-29 20:56:06 -0500

    added extract_markdown images and links

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from textnode import *
 from htmlnode import *
+from markdown import *
 
 def main():
     
-    node = TextNode("This is text with a `code block` word", text_type_text)
-    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
-    print(new_nodes)
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    print(extract_markdown_images(text))
+    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    print(extract_markdown_links(text))
+    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
 main()
 
