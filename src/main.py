d4ef4039961326523188b961db56476aef9f8b03
amuffler10 <austinmuffler@yahoo.com>
2024-08-29 19:29:05 -0500

    added split_nodes_delimiter

--- src/main.py
+++ src/main.py
@@ -1,18 +1,10 @@
-from textnode import TextNode
+from textnode import *
 from htmlnode import *
 
 def main():
-    node = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-)
-
-    print(node.to_html())
-
+    
+    node = TextNode("This is text with a `code block` word", text_type_text)
+    new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    print(new_nodes)
 main()
 
